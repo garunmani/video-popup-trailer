c899106ff3288ad34edc4d33463e6dfa2f6a1d0d
garunmani <121419738+garunmani@users.noreply.github.com>
2022-12-30 20:21:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const btnE1 =document.querySelector(".btn");
+const trailerE1=document.querySelector(".video-trailer");
+
+
+btnE1.addEventListener("click", ()=>{
+    trailerE1.classList.remove("active");
+});
+
